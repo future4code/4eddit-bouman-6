f7afc95b028fe17e74d2475b755217cba7f7bb57
lucas constantino <l_campioto@hotmail.com>
2020-01-20 14:34:16 -0300

    Finalizando a tela de login

---
+++ 4eddit-boilerplate/src/actions/login.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { push } from "connected-react-router";
+import { routes } from "../containers/Router/index";
+
+export const login = (email, password) => async (dispatch) =>{
+    const loginInformation = {
+        email,
+        password,
+    }
+
+    try {
+        const response = await axios.post("https://us-central1-missao-newton.cloudfunctions.net/fourEddit/login", loginInformation);
+        window.localStorage.setItem("token", response.data.token);
+
+        dispatch(push(routes.feed))
+        
+    } catch(error) {
+        window.alert("Login Invalido")
+    }
+}
